a23bca3a450bddc8a2bad114ef5daa994921fa7d
RaoulBock <anthonyraoulbock01@gmail.com>
2022-02-08 08:55:13 +0200

    API

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import News from "./screens/News";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator
+} from "react-native";
+
+const stackNavigator = createStackNavigator({
+  News: News
 });
+
+const App = createAppContainer(stackNavigator);
+export default App;
---
+++ screens/News.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, StyleSheet, Text, Button, SafeAreaView } from "react-native";
+
+const News = ({ navigation }) => {
+  return (
+    <SafeAreaView>
+      <Text>News Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default News;
