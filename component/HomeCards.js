3dc39c4c4c9de43ccefd30293de09bbe78b76998
RaoulBock <anthonyraoulbock01@gmail.com>
2022-02-14 13:19:35 +0200

    front end

--- component/HomeCards.js
+++ component/HomeCards.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
 import React from "react";
 
-const HomeCards = () => {
+const { width, height } = Dimensions.get("window");
+
+const HomeCards = ({ item, navigation }) => {
   return (
-    <View>
-      <Text>HomeCards</Text>
+    <View style={{ marginTop: "4%" }}>
+      <View
+        style={{ backgroundColor: "white", padding: "4%", borderRadius: 8 }}
+      >
+        <Text>{item.publishedAt}</Text>
+        <Image style={styles.image} source={{ uri: item.urlToImage }} />
+        <Text>{item.title}</Text>
+        <Text>{item.description}</Text>
+      </View>
     </View>
   );
 };
 
 export default HomeCards;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: height / 4,
+    borderRadius: 8,
+    marginTop: "4%",
+    marginBottom: "2%"
+  }
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
 import {
+  StyleSheet,
+  Text,
+  View,
   FlatList,
   ScrollView,
   TouchableOpacity,
   Image,
-  SafeAreaView
-} from "react-native-gesture-handler";
+  SafeAreaView,
+  TextInput
+} from "react-native";
+import React, { useState, useEffect } from "react";
 
 // import { Entypo } from "react-native-vector-icons";
 import HomeCards from "../component/HomeCards";
+
+import newAPI from "../api/News";
 
 const HomeScreen = ({ navigation }) => {
   const menus = [
     {
       id: "1",
-      bigTitle: "For you",
-      screen: "HomeScreen"
+      screen: "HomeScreen",
+      bigTitle: "For you"
     },
     {
       id: "2",
@@ -34,37 +39,103 @@ const HomeScreen = ({ navigation }) => {
       bigTitle: "Post"
     }
   ];
+  const [news, setNews] = useState([]);
+  const [search, setSearch] = useState("");
+  const NothingFound = require("../assets/nothingFound.gif");
+
+  useEffect(() => {
+    getNewsFromAPI();
+  }, []);
+
+  useEffect(() => {}, [news]);
+
+  function getNewsFromAPI() {
+    newAPI
+      .get(
+        "top-headlines?country=us&apiKey=245b4ed876e344d0a083dc5c4604eba9&category=business"
+      )
+      .then(async function (response) {
+        setNews(response.data);
+        console.log(news);
+      })
+      .catch(function (error) {
+        // console.log(error);
+      });
+
+    // console.log(searchNews);
+  }
+  let searchNews = [];
+  if (!news) {
+    return null;
+  } else if (news && news.articles) {
+    searchNews = news.articles.filter((e) => {
+      return `${e.title || e.description}`
+        .toLowerCase()
+        .includes(search.toString().toLowerCase());
+    });
+  }
 
   return (
     <SafeAreaView style={{ backgroundColor: "#f9f9fb", height: "100%" }}>
-      <ScrollView>
+      <View style={{ marginTop: "4%" }}>
+        <FlatList
+          data={menus}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => navigation.navigate(item.screen)}
+              style={{ paddingHorizontal: 33, textAlign: "center" }}
+            >
+              <View>
+                <Text style={{ color: "black", fontWeight: "700" }}>
+                  {item.bigTitle}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+      <View style={styles.top}>
         <View>
+          <Text style={styles.title}>Welcome</Text>
+        </View>
+        <TextInput
+          placeholder="Search here ..."
+          type="search"
+          style={styles.search}
+          onChangeText={(e) => {
+            setSearch(e);
+          }}
+        />
+        {searchNews.length > 0 && (
           <FlatList
-            data={menus}
-            keyExtractor={(item) => item.id}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                onPress={() => navigation.navigate(item.screen)}
-                style={{ paddingHorizontal: 33, textAlign: "center" }}
-              >
-                <View>
-                  <Text style={{ color: "black", fontWeight: "700" }}>
-                    {item.bigTitle}
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            )}
+            data={searchNews}
+            keyExtractor={(item, index) => "key" + index}
+            renderItem={({ item }) => {
+              return <HomeCards item={item} />;
+            }}
           />
-        </View>
-        <View style={styles.top}>
-          <View>
-            <Text style={styles.title}>Welcome</Text>
+        )}
+        {searchNews.length === 0 && (
+          <View
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              flex: 1
+            }}
+          >
+            <Image
+              source={NothingFound}
+              style={{
+                width: 200,
+                height: 200
+              }}
+            />
           </View>
-          <HomeCards />
-        </View>
-      </ScrollView>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
@@ -88,5 +159,11 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 38
+  },
+  search: {
+    backgroundColor: "white",
+    padding: "2%",
+    margin: "4%",
+    borderRadius: 8
   }
 });
